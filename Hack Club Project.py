159c72ae14d0ce5b860467bf8ec66e068789ea36
MouseMouse625 <134831967+MouseMouse625@users.noreply.github.com>
2024-07-03 10:32:37 +0800

    Add files via upload

--- Hack Club Project.py
+++ Hack Club Project.py
@@ -26,59 +26,72 @@ class Text(py.sprite.Sprite):
         self.rect.center = (self.xPosition , self.yPosition)
 def comfortaa(size):
     return py.font.Font("Comfortaa-Light.ttf" , (size))
-def append(prefix , suffix , pluralMaker):
+def append(prefix , singularSuffix , pluralSuffix):
     if prefix == 1:
-        return str(prefix) + " " + str(suffix)
+        return str(prefix) + " " + str(singularSuffix)
     else:
-        return str(prefix) + " " + str(suffix) + str(pluralMaker)
+        return str(prefix) + " " + str(pluralSuffix)
 screenWidth = 500
 screenHeight = 500
 dotValue = 0
-dotCount = append(dotValue , "Dot" , "s")
+dotCount = append(dotValue , "Dot" , "Dots")
 clickValue = 1
+clickCount = append(clickValue , "DPC" , "DPC")
 clickUpgradeValue = 0
-prestigeValue = 0
+prestigeValue = 2
+prestigeCount = append(prestigeValue , "Prestige" , "Prestiges")
+stageValue = 1
 screen = py.display.set_mode((screenWidth , screenHeight))
 py.display.set_icon(py.image.load("Window Icon.png"))
 py.display.set_caption("Hack Club Project")
 dotButton = Button(30 , 30 , 20 , 20 , (0 , 200 , 255))
 clickUpgradeButton = Button(30 , 30 , 20 , 60 , (200 , 255 , 0))
 prestigeButton = Button(30 , 30 , 20 , 100 , (255 , 0 , 0))
+stagePrestigeButton = Button(30 , 30 , 20 , 140 , (0 , 255 , 20))
 dotCounter = Text(dotCount , 440 , 20 , (255 , 255 , 255) , 25)
+clickCounter = Text(clickCount , 440 , 60 , (255 , 255 , 255) , 25)
+prestigeCounter = Text(prestigeCount , 70 , 480 , (255 , 255 , 255) , 25)
 text = py.sprite.Group()
-text.add(dotCounter)
+text.add(dotCounter , clickCounter , prestigeCounter)
 programRunning = True
 while programRunning:
     for event in py.event.get():
-        if event.type == py.QUIT:
-            programRunning = False
-        elif event.type == py.MOUSEBUTTONDOWN:
-            if mouseX > dotButton.rect.left and mouseX < dotButton.rect.right and mouseY > dotButton.rect.top and mouseY < dotButton.rect.bottom:
-                dotValue = dotValue + clickValue
-            if mouseX > clickUpgradeButton.rect.left and mouseX < clickUpgradeButton.rect.right and mouseY > clickUpgradeButton.rect.top and mouseY < clickUpgradeButton.rect.bottom:
-                if dotValue > 10 ** (clickUpgradeValue + 2):
-                    clickValue = (clickValue + 1) * (prestigeValue + 1)
-                    clickUpgradeValue = clickUpgradeValue + 1
-            if mouseX > prestigeButton.rect.left and mouseX < prestigeButton.rect.right and mouseY > prestigeButton.rect.top and mouseY < prestigeButton.rect.bottom:
-                if prestigeValue == 0:
-                    if dotValue > 15000:
+        if stageValue == 1:
+            if event.type == py.QUIT:
+                programRunning = False
+            elif event.type == py.MOUSEBUTTONDOWN:
+                if mouseX > dotButton.rect.left and mouseX < dotButton.rect.right and mouseY > dotButton.rect.top and mouseY < dotButton.rect.bottom:
+                    dotValue = dotValue + clickValue
+                if mouseX > clickUpgradeButton.rect.left and mouseX < clickUpgradeButton.rect.right and mouseY > clickUpgradeButton.rect.top and mouseY < clickUpgradeButton.rect.bottom:
+                    if dotValue > 10 ** (clickUpgradeValue + 2):
+                        clickValue = (clickValue + 1) * (prestigeValue + 1)
+                        clickUpgradeValue = clickUpgradeValue + 1
+                if mouseX > prestigeButton.rect.left and mouseX < prestigeButton.rect.right and mouseY > prestigeButton.rect.top and mouseY < prestigeButton.rect.bottom:
+                    if dotValue > (15000 * ((prestigeValue + 1) ** prestigeValue)):
                         prestigeValue = prestigeValue + 1
                         dotValue = 0
                         clickValue = 1
                         clickUpgradeValue = 0
-    screen.fill((255 , 0 , 255))
-    mouseX , mouseY = py.mouse.get_pos()
-    text.empty()
-    dotCount = append(dotValue , "Dot" , "s")
-    dotCounter = Text(dotCount , 440 , 20 , (255 , 255 , 255) , 25)
-    text.add(dotCounter)
-    screen.blit(dotButton.surface , dotButton.rect)
-    if dotValue > 10 ** (clickUpgradeValue + 2):
-        screen.blit(clickUpgradeButton.surface , clickUpgradeButton.rect)
-    if prestigeValue == 0:
-        if dotValue > 15000:
+    if stageValue == 1:
+        screen.fill((255 , 0 , 255))
+        mouseX , mouseY = py.mouse.get_pos()
+        text.empty()
+        dotCount = append(dotValue , "Dot" , "Dots")
+        dotCounter = Text(dotCount , 440 , 20 , (255 , 255 , 255) , 25)
+        clickCount = append(clickValue , "DPC" , "DPC")
+        clickCounter = Text(clickCount , 440 , 60 , (255 , 255 , 255) , 25)
+        prestigeCount = append(prestigeValue , "Prestige" , "Prestiges")
+        prestigeCounter = Text(prestigeCount , 70 , 480 , (255 , 255 , 255) , 25)
+        text.add(dotCounter , clickCounter , prestigeCounter)
+        screen.blit(dotButton.surface , dotButton.rect)
+        if dotValue > 10 ** (clickUpgradeValue + 2):
+            screen.blit(clickUpgradeButton.surface , clickUpgradeButton.rect)
+        if dotValue > (15000 * ((prestigeValue + 1) ** prestigeValue)):
             screen.blit(prestigeButton.surface , prestigeButton.rect)
-    for textbox in text:
-        screen.blit(textbox.surface , textbox.rect)
+        if prestigeValue > 9 and dotCount > ((15000 * ((prestigeValue + 1) ** 1.8)) * 0.75):
+            screen.blit(stagePrestigeButton.surface , stagePrestigeButton.rect)
+        for textbox in text:
+            screen.blit(textbox.surface , textbox.rect)
+        print((15000 * ((prestigeValue + 1) ** prestigeValue)))
     py.display.flip()
 py.quit()
